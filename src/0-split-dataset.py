2f79c8713076175c34c8d86263411f172e70dd88
Sergio <sergiodfc@gmail.com>
2025-05-15 06:27:39 -0500

    Corrige rutas relativas en 0-split-dataset

--- src/0-split-dataset.py
+++ src/0-split-dataset.py
@@ -9,15 +9,15 @@ def split_data(df_data, perc_data_train):
     return df_data_train, df_data_test
 
 def process_split_data():
-    print(os.getcwd())
-    #os.chdir("..")
-    #print(os.getcwd())
-    df_data = pd.read_csv("../data/in/application_data.csv")
+    curr_path = os.getcwd()
+    if (curr_path.endswith('src')):
+        os.chdir("..")
+    df_data = pd.read_csv("data/in/application_data.csv")
     df_data_train, df_data_test = split_data(df_data, 0.7)
-    if not (os.path.exists("../data/out")):
-        os.mkdir("../data/out")
-    df_data_train.to_csv("../data/out/application_data_train.csv", index=False)
-    df_data_test.to_csv("../data/out/application_data_test.csv", index=False)
+    if not (os.path.exists("data/out")):
+        os.mkdir("data/out")
+    df_data_train.to_csv("data/out/application_data_train.csv", index=False)
+    df_data_test.to_csv("data/out/application_data_test.csv", index=False)
 
 def main():
     process_split_data()
